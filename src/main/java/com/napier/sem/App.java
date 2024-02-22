c1dbb69bf25369e07eecf71190facb54b95c41aa
Patrick Mountcastle <pmountcastle1@vt.edu>
2024-02-22 14:48:45 +0000

    Loaded in all salaries from the database.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -12,10 +12,13 @@ public class App
 
         // Connect to database
         a.connect();
-        // Get Employee
-        Employee emp = a.getEmployee(255530);
-        // Display results
-        a.displayEmployee(emp);
+
+        // Extract employee salary information
+        ArrayList<Employee> employees = a.getAllSalaries();
+
+        // Test the size of the returned data - should be 240124
+        System.out.println(employees.size());
+
         // Disconnect from database
         a.disconnect();
     }
